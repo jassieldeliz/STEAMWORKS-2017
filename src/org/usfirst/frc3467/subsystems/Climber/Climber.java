4ac2e7430c0a7961c184ec97900bbe1a40009fb7
Nick <npessinis@team3467.org>
2017-02-11 13:56:33 -0500

    New hopper files

--- src/org/usfirst/frc3467/subsystems/Climber/Climber.java
+++ src/org/usfirst/frc3467/subsystems/Climber/Climber.java
@@ -2,18 +2,42 @@
 package org.usfirst.frc3467.subsystems.Climber;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.CANTalon;
+import org.usfirst.frc3467.robot.RobotMap;
 
 /**
  *
  */
 public class Climber extends Subsystem {
-    
+    // Controls display SmartDashboard
+	private static final boolean debugging = true;
+	//Constants for speed
+	public static final double kClimb = 0.7;
+	public static final double kLower = -0.3;
+	public static final double kStop = 0;
+
+	//Roller class objects
+	public CANTalon climbMotor;
+
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+	public Climber(){
+		climbMotor = new CANTalon(RobotMap.climbMotor);
+	}
     public void initDefaultCommand() {
+
         // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        //setDefaultCommand(new MySpecialCommand()); 
+    	this.setDefaultCommand(new ClimberDrive());
     }
+	public void driveAuto(double speed) {
+		// TODO Auto-generated method stub
+		if (debugging) {
+	    	SmartDashboard.putNumber("Climbing Speed", speed);
+		}
+		climbMotor.set(speed);
+		
+	}
 }
 
---
+++ src/org/usfirst/frc3467/subsystems/Climber/ClimberDrive.java
@@ -0,0 +1,68 @@
+package org.usfirst.frc3467.subsystems.Climber;
+
+import org.usfirst.frc3467.robot.CommandBase;
+import org.usfirst.frc3467.subsystems.Climber.Climber;;
+
+public class ClimberDrive extends CommandBase {
+
+	private double velocity;
+	
+	public ClimberDrive(){
+		requires(climber);
+		velocity = 0;
+	}
+	public ClimberDrive(double speed){
+		requires(climber);	
+		velocity = speed;
+	}
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		double speed = 0;
+		
+		if(velocity == 0){
+			//speed = oi.getPrime(Arcade,1);
+			
+			//deadband
+			if (speed > -0.08 && speed < 0.08) speed = 0;
+	       
+			// Square the inputs (while preserving the sign) to increase
+			// fine control while permitting full power
+		if (speed>= 0.0)speed = (speed*speed);
+		else speed = -(speed*speed);
+		}
+		else{
+			if (velocity == Climber.kClimb){
+				speed = 0;
+			}
+			else{
+				speed = velocity;
+			}
+			climber.driveAuto(speed);
+		}
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		climber.driveAuto(0);
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		end();
+	}
+
+}
