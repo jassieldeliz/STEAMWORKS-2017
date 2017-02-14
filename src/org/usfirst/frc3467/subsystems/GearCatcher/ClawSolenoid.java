27f41b12715a0d3e12818ef1f1b3af7646393374
jassieldeliz <jassieldeliz@gmail.com>
2017-02-14 17:56:21 -0500

    Claw Code

---
+++ src/org/usfirst/frc3467/subsystems/GearCatcher/ClawSolenoid.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team3467.robot.claw;
+
+public class Solenoid {
+	{
+	Solenoid Solenoid = new Solenoid();
+	Solenoid = new Solenoid();
+	Solenoid.set(true);
+	Solenoid.set(false);
+}
+	public Object get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void set(boolean b) {
+		
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
