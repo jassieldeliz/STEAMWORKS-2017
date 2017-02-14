965f7157825996d6c1404eac8fca046b723793e2
scottkukshtel <scottkukshtel@team3467.org>
2017-02-14 16:34:02 -0500

    Organize Robot Map

--- src/org/usfirst/frc3467/robot/RobotMap.java
+++ src/org/usfirst/frc3467/robot/RobotMap.java
@@ -6,26 +6,16 @@ package org.usfirst.frc3467.robot;
  * floating around.
  */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
 	
 	// Pneumatics
 	public static final int pneumatics_sensor_port = 1;
 
 	// Drivebase
-	
 	//CANTalon Ports (best to not start with 1, because that is the number assigned to a CANTalon by default)
 	public static final int drivebase_LeftTalon = 2;
 	public static final int drivebase_RightTalon = 5;
 	public static final int drivebase_CenterTalon = 9;
-	public static final int climbMotor = 0;
+
 	// Slave CANTalons
 	public static final int drivebase_LeftTalon2 = 3;
 	public static final int drivebase_LeftTalon3 = 4;
@@ -33,9 +23,14 @@ public class RobotMap {
 	public static final int drivebase_RightTalon3 = 7;
 	public static final int drivebase_CenterTalon2 = 10;
 
-	public static final int hopper_solenoid_retract = 0;
+	// Climber
+	public static final int climbMotor = 9;
 
-	public static final int hopper_solenoid_deploy = 0;
+	//Solenoid mappings
+	
+	// Hopper 
+	public static final int hopper_solenoid_retract = 0;
+	public static final int hopper_solenoid_deploy = 1;
 
 	
 	
