adaaa67a5a3bf9334183dc36cf57d648dfc16382
jassieldeliz <jassieldeliz@gmail.com>
2017-02-17 15:38:33 -0500

    Claw catcher

---
+++ src/org/usfirst/frc3467/subsystems/GearCatcher/ClawSolenoid.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc3467.subsystems.GearCatcher; 
+
+public class ClawSolenoid {
+	{
+	ClawSolenoid Solenoid = new ClawSolenoid();
+	Solenoid = new ClawSolenoid();
+	Solenoid.set(true);
+	Solenoid.set(false);
+}
+	public Object get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void set(boolean b) {
+		
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
